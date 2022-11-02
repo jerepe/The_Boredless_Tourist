2b7a65ae2ba7af63128767a21c948aeffbb7e88e
j0j0 <74825770+jerepe@users.noreply.github.com>
2022-11-02 17:07:31 +0100

    Added interest finder logic

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 from importlib.abc import Traversable
 
-
 destinations = [
     'Paris, France',
     'Shanghai, China',
@@ -14,27 +13,28 @@ test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index
-print(get_destination_index('Shanghai, China'))
 
 def get_traveler_location(traveler):
-    traveler_destination = traveler[1]
-    traveler_destination_index = get_destination_index(traveler_destination)
+    # we model our example on test_traveler, as all will be formatted in the same way:
+    traveler_destination = traveler[1] # traveler_destination = 'Shanghai, China'
+    traveler_destination_index = get_destination_index(traveler_destination) # traveler_destination_index = get_destination_index('Shanghai, China') = destinations.index('Shanghai, China') = 1
     return traveler_destination_index
 
-test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+test_destination_index = get_traveler_location(test_traveler) # = traveler_destination_index = 1
 
-attractions = [[] for destination in destinations]
-print(attractions)
+attractions = [[] for destination in destinations] # = [[], [], [], [], []]
 
 def add_attraction(destination, attraction):
-    destination_index = get_destination_index(destination)
-    attractions_for_destination = attractions[destination_index]
-    attractions_for_destination.append(attraction)
+    destination_index = get_destination_index(destination) # lets say here destination_index = 2
+    # we locate the attractions_for_destination as being in one list of the attractions list, located at the index previously mentionned:
+    attractions_for_destination = attractions[destination_index] # = attractions[2]
+    # we append to this new list placed at attractions[2], the attraction passed in arguments of add_attraction(destination, attraction)
+    attractions_for_destination.append(attraction) 
     return attractions
-print(attractions)
+
 add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
 print(attractions)
+
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
@@ -45,3 +45,19 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
+
+la_arts = find_attractions('Los Angeles, USA', ['art'])
+print(la_arts)
+
